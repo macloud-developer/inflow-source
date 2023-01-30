f0b4615848fa7f481060e183c625557d989c92c3
r-eiyama <r-eiyama@macloud.jp>
2023-01-30 15:04:27 +0900

    テストかけた

--- test/date.spec.ts
+++ test/date.spec.ts
@@ -15,4 +15,27 @@ describe('~/date', () => {
       expect(date.toString()).toBe('Sun, 06 Dec 2015 15:00:00 GMT')
     })
   })
+  test('hasElapsedOneHour_true', () => {
+    const currentDate = useDate().create('2023-01-30 13:00:00')
+    const targetDate = useDate().create('2023-01-30 12:00:00')
+    expect(useDate().hasElapsedOneHour(currentDate, targetDate)).toBeTruthy()
+  })
+
+  test('hasElapsedOneHour_false', () => {
+    const currentDate = useDate().create('2023-01-30 13:00:00')
+    const targetDate = useDate().create('2023-01-30 12:00:01')
+    expect(useDate().hasElapsedOneHour(currentDate, targetDate)).toBeFalsy()
+  })
+
+  test('hasElapsedOneHour_false2', () => {
+    const currentDate = useDate().create('2023-01-30 13:00:00')
+    const targetDate = useDate().create('2023-01-30 13:00:00')
+    expect(useDate().hasElapsedOneHour(currentDate, targetDate)).toBeFalsy()
+  })
+
+  test('hasElapsedOneHour_error', () => {
+    const currentDate = useDate().create('2023-01-30 13:00:00')
+    const targetDate = useDate().create('2023-01-30 13:00:01')
+    expect(() => useDate().hasElapsedOneHour(currentDate, targetDate)).toThrowError(new Error('rawCurrentDateより未来に日時をtargetDateに指定できません'))
+  })
 })
